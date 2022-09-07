31511c447b4d1e7346d6b292509cd874310dc1ae
Samantha Gold <sgold@ripl.org>
2022-09-07 14:48:31 -0400

    latest push

--- 1_create_corpus.py
+++ 1_create_corpus.py
@@ -10,6 +10,7 @@ import nltk
 import os
 import pandas as pd
 from datetime import datetime
+import ast 
 
 # Setup 
 dir = "Downloads/delidata/"
@@ -40,6 +41,28 @@ delidata_annotated.columns = delidata_annotated.columns.str.lower()
 delidata_all['message_id'].nunique()       #36,937
 delidata_annotated['message_id'].nunique() #1,696: annotated is subset of all raw data
 
+# Generating utterances
+# Extract utterances
+deli_utt = delidata_all[delidata_all['message_type']=='CHAT_MESSAGE']
+print(len(deli_utt)) #14,035
+#%%
+#Adjusting ast.literal_eval: There are some utterances covered with ""
+deli_content_processed = []
+for i in range(len(deli_utt)):
+  if "{'message'" in deli_utt['content'].iloc[i]:
+    a = ast.literal_eval(deli_utt['content'].iloc[i])['message']
+    #print(a)
+  elif isinstance(deli_utt['content'].iloc[i], dict):
+    a = deli_utt['content'].iloc[i]['message']
+  else:
+    a = deli_utt['content'].iloc[i]
+  deli_content_processed.append(a)
+#%%
+deli_utt['text'] = deli_content_processed
+deli_utt = deli_utt.drop(['content'],axis=1)
+#%%
+#Select required columns
+deli_utt = deli_utt[['message_id', 'user_name', 'user_id','timestamp','convo_id','text']].rename(columns={'user_id':'speaker','convo_id':'conversation_id'}).reset_index(drop=True)
 
                     
 # Additional cleaning steps I think we need to do before we can convert to corpus
@@ -54,12 +77,18 @@ delidata_annotated['message_id'].nunique() #1,696: annotated is subset of all ra
 # STEP 2: POPULATE REPLY_TO FIELD
 delidata_messages = delidata_all[delidata_all['message_type'] == "CHAT_MESSAGE"]
 ## a: convert timestamp to datetime 
-delidata_messages['timestamp2'] = pd.to_datetime(delidata_messages['timestamp'])
+delidata_utt = deli_utt
+delidata_utt['timestamp2'] = pd.to_datetime(delidata_utt['timestamp'])
 ## b: sort timestamp, by convo to get correct conversation ordering
-delidata_messages = delidata_messages.sort_values(['timestamp2'], ascending = True).groupby('convo_id')
+delidata_utt = delidata_utt.sort_values(['timestamp2'], ascending = True).groupby('conversation_id')
 ##c: create lag of message_id, rename it reply_to 
-delidata_messages = delidata_messages.apply(lag, column = 'message_id').sort_values(['convo_id', 'timestamp2'], ascending = True)
-delidata_messages.rename(columns={'lagged_message_id':'reply_to'}, inplace = True)
+delidata_utt = delidata_utt.apply(lag, column = 'message_id').sort_values(['conversation_id', 'timestamp2'], ascending = True)
+delidata_utt = delidata_utt.rename(columns={'lagged_message_id':'reply_to'}, inplace = True)
+deli_utt = delidata_utt[['message_id', 'speaker', 'timestamp', 'conversation_id', 'text', 'reply_to']].rename(columns={'message_id':'id'})
+deli_utt = deli_utt[['id', 'timestamp', 'text', 'speaker', 'reply_to', 'conversation_id']]
+
+
+
 
 ##############Corpus Construction Notes 
 # Notes about a corpus: 
@@ -79,3 +108,12 @@ def parse_meta(val):
 delidata_user['meta.finalanswer']=delidata_user['content'].apply(parse_meta)
 delidata_user = delidata_user.sort_values(['timestamp'], ascending = True).groupby(['convo_id', 'user_id']).tail(1)
 user_df = delidata_user[['user_id', 'meta.finalanswer']]
+user_df = user_df.rename(columns={'user_id': 'id'})
+
+convo_df = delidata_all.convo_id.drop_duplicates().to_frame()
+convo_df = convo_df.rename(columns={'convo_id':'id'})
+# Gathering all components of the corpus
+
+
+delidata_corpus = Corpus.from_pandas(utterances_df=deli_utt, speakers_df=user_df, conversations_df=convo_df)
+
